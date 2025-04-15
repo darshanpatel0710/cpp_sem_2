9996e940f779261d74c721ee02357b4eac14f29f
darshanpatel0710 <dsp071006@gmail.com>
2025-04-15 10:56:08 +0530

    1_1.cpp

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,74 +1,78 @@
 #include <iostream>
 using namespace std;
 
-class BankAccount {
-private:
-    char accountHolderName[100];
-    int accountNumber;
-    double balance;
+class bank_account 
+{
+
+    char account_holder_name[100];
+    int account_number;
+    double account_balance;
 
 public:
-    void setAccountDetails(const char* name, int number, double initialBalance) {
+    void set_account_details(const char* holder_name, int acc_number, double initial_balance) {
         int i = 0;
-        while (name[i] != '\0' && i < 99) {
-            accountHolderName[i] = name[i];
+        while (holder_name[i] != '\0' && i < 99) {
+            account_holder_name[i] = holder_name[i];
             i++;
         }
-        accountHolderName[i] = '\0';
-        accountNumber = number;
-        balance = initialBalance;
+        account_holder_name[i] = '\0';
+        account_number = acc_number;
+        account_balance = initial_balance;
     }
 
-    void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            cout << "Deposit successful. Current balance: $" << balance << endl;
+    void deposit_amount(double deposit_amount) {
+        if (deposit_amount > 0) {
+            account_balance += deposit_amount;
+            cout << "Deposit successful. Current balance: $" << account_balance << endl;
         }
     }
 
-    void withdraw(double amount) {
-        if (amount > balance) {
+    void withdraw_amount(double withdraw_amount) {
+        if (withdraw_amount > account_balance) {
             cout << "Insufficient funds" << endl;
-        } else if ((balance - amount) < 500) {
+        } else if ((account_balance - withdraw_amount) < 500) {
             cout << "Cannot withdraw. Minimum balance of 500 must be maintained." << endl;
         } else {
-            balance -= amount;
-            cout << "Withdrawal successful. Current balance: $" << balance << endl;
+            account_balance -= withdraw_amount;
+            cout << "Withdrawal successful. Current balance: $" << account_balance << endl;
         }
     }
 
-    void checkBalance() {
-        cout << "Account Number: " << accountNumber << endl;
-        cout << "Account Holder: " << accountHolderName << endl;
-        cout << "Balance: $" << balance << endl;
+    void check_account_balance() {
+        cout << "Account Number: " << account_number << endl;
+        cout << "Account Holder: " << account_holder_name << endl;
+        cout << "Balance: $" << account_balance << endl;
     }
 };
 
 int main() {
-    BankAccount account;
-    account.setAccountDetails("John Doe", 12345, 1000.0);
-    int choice;
-    double amount;
+    bank_account user_account;
+    user_account.set_account_details("John Doe", 12345, 1000.0);
+    
+    int user_choice;
+    double transaction_amount;
+    
     do {
         cout << "\n1. Deposit" << endl;
         cout << "2. Withdraw" << endl;
         cout << "3. Check Balance" << endl;
         cout << "4. Exit" << endl;
         cout << "Enter your choice: ";
-        cin >> choice;
-        switch (choice) {
+        cin >> user_choice;
+
+        switch (user_choice) {
             case 1:
                 cout << "Enter amount to deposit: ";
-                cin >> amount;
-                account.deposit(amount);
+                cin >> transaction_amount;
+                user_account.deposit_amount(transaction_amount);
                 break;
             case 2:
                 cout << "Enter amount to withdraw: ";
-                cin >> amount;
-                account.withdraw(amount);
+                cin >> transaction_amount;
+                user_account.withdraw_amount(transaction_amount);
                 break;
             case 3:
-                account.checkBalance();
+                user_account.check_account_balance();
                 break;
             case 4:
                 cout << "Exiting..." << endl;
@@ -76,7 +80,8 @@ int main() {
             default:
                 cout << "Invalid choice" << endl;
         }
-    } while (choice != 4);
-   cout<<"24CE076_PatelDarshan";
+    } while (user_choice != 4);
+
+    cout << "24CE076_PatelDarshan";
     return 0;
 }
